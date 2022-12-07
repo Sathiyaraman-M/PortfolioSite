17bffc302199df56310f14c01777e4f3a825a1ea
SathiyaramanM <sathiyaraman2003@gmail.com>
2022-12-07 15:34:08 +0530

    Added new projects in the Portfolio

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -4,6 +4,8 @@ import PortfolioModal from "./PortfolioModal";
 import cabin from '../assets/portfolio/cabin.png'
 import cake from '../assets/portfolio/cake.png'
 import game from '../assets/portfolio/game.png'
+import safe from '../assets/portfolio/safe.png'
+import submarine from '../assets/portfolio/submarine.png'
 
 const Modals = () => {
     return (
@@ -23,13 +25,25 @@ const Modals = () => {
                     A Hardware Assets Inventory application, capable of tracking hardware components change, again fully built on .NET using Blazor WebAssembly, ASP.NET Core, SQL Server. Built as a part of <b>Smart India Hackathon 2022</b>.
                 </>
             } link="https://github.com/Sathiyaraman-M/Assets_Inventory" />
-            <PortfolioModal id="infinite" title="Novel Infinite" image={game} content={
+            <PortfolioModal id="resurgence" title="Visitor Management System" image={game} content={
+                <>
+                    <i><b>Code Name: </b> Resurgence</i>
+                    <br/>
+                    A Proper Visitor Management System, that tracks visitors in/out using Visitor passes, again fully built on .NET using Blazor WebAssembly, ASP.NET Core, SQL Server. This project is currently in production.
+                </>
+            } link="https://github.com/Sathiyaraman-M/Resurgence" />
+            <PortfolioModal id="infinite" title="Novel Infinite" image={safe} content={
                 <>
                     <i><b>Code Name: </b> Novel Infinite</i>
                     <br/>
                     An Online Social Platform to share projects and blogs, again fully built on .NET using Blazor WebAssembly, ASP.NET Core, SQL Server.
                 </>
             } link="https://github.com/Sathiyaraman-M/Infinite_M2" />
+            <PortfolioModal id="portfolio-react" title="Portfolio Site" image={submarine} content={
+                <>
+                    The Site currently you are seeing, is my Portfolio site, also my first ReactJS project.
+                </>
+            } link="https://github.com/Sathiyaraman-M/PortfolioSite" />
     </>
     );
 };
--- src/sections/Portfolio.js
+++ src/sections/Portfolio.js
@@ -3,6 +3,8 @@ import cabin from '../assets/portfolio/cabin.png'
 import cake from '../assets/portfolio/cake.png'
 import circus from '../assets/portfolio/circus.png'
 import game from '../assets/portfolio/game.png'
+import safe from '../assets/portfolio/safe.png'
+import submarine from '../assets/portfolio/submarine.png'
 
 const Portfolio = () => (
     <section className="page-section portfolio" id="portfolio">
@@ -45,13 +47,33 @@ const Portfolio = () => (
                     </div>
                 </div>
                 <div className="col-md-6 col-lg-4 mb-5">
-                    <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#infinite">
+                    <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#resurgence">
                         <div
                             className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                             <div className="portfolio-item-caption-content text-center text-white"><i
                                 className="fas fa-plus fa-3x"></i></div>
                         </div>
                         <img className="img-fluid" src={game} alt="..."/>
+                    </div>
+                </div>
+                <div className="col-md-6 col-lg-4 mb-5">
+                    <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#infinite">
+                        <div
+                            className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                            <div className="portfolio-item-caption-content text-center text-white"><i
+                                className="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img className="img-fluid" src={safe} alt="..."/>
+                    </div>
+                </div>
+                <div className="col-md-6 col-lg-4 mb-5">
+                    <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolio-react">
+                        <div
+                            className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                            <div className="portfolio-item-caption-content text-center text-white"><i
+                                className="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img className="img-fluid" src={submarine} alt="..."/>
                     </div>
                 </div>
                 {/*<div className="col-md-6 col-lg-4 mb-5">*/}
