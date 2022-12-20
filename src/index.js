2dcef9b2d4bfd9af5de3ea6449b3c07c9241ff9f
SathiyaramanM <sathiyaraman2003@gmail.com>
2022-12-20 07:14:38 +0530

    Added Routing ability using react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from "./App";
+import {BrowserRouter} from "react-router-dom";
 
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+    <BrowserRouter basename={baseUrl}>
+        <App />
+    </BrowserRouter>
+);
